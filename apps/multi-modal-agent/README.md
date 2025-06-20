e78d9ede5f41c46350891518e1e9c336c62a8d53
Ke Xiao <ke.xiao@bytedance.com>
2025-06-20 10:30:38 -0700

    chore: add apps folder

---
+++ apps/multi-modal-agent/README.md
@@ -0,0 +1,27 @@
+# multi-modal-agent
+
+## Get Started
+
+Start the dev server:
+
+```
+pnpm dev
+```
+
+Enable optional features or add a new entry:
+
+```
+pnpm new
+```
+
+Build the app for production:
+
+```
+pnpm build
+```
+
+Preview the production build locally:
+
+```
+pnpm serve
+```
---
+++ apps/multi-modal-agent/edenx.config.ts
@@ -0,0 +1,14 @@
+import { appTools, defineConfig } from '@edenx/app-tools';
+
+// https://edenx.bytedance.net/configure/app/usage
+export default defineConfig({
+  runtime: {
+    router: true,
+  },
+  devtools: { sw: false },
+  plugins: [
+    appTools({
+      bundler: 'rspack', // Set to 'webpack' to enable webpack
+    }),
+  ],
+});
