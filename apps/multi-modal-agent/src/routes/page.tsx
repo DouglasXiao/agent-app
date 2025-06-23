b9101c4b99888b7b4800e40d98e8f0283b39524c
Ke Xiao <ke.xiao@bytedance.com>
2025-06-23 13:03:15 -0700

    chore: change logo

--- apps/multi-modal-agent/src/routes/page.tsx
+++ apps/multi-modal-agent/src/routes/page.tsx
@@ -1,9 +1,18 @@
 import { ChatApp } from '@/features/chat/ChatApp';
+import { Helmet } from '@edenx/runtime/head';
 import './chat.css';
 import '../styles/globals.css';
 
+export const AutopeLogo =
+  'https://lf3-static.bytednsdoc.com/obj/eden-cn/lm_sth/ljhwZthlaukjlkulzlp/autope/logo/prompt-pilot.png';
+
 const Index = (): JSX.Element => (
   <div className="w-full h-screen">
+    <Helmet>
+      <link rel="icon" href={AutopeLogo} />
+      <title>MultiModalAgents</title>
+      <meta name="description" content="This is a multi-modal agent app" />
+    </Helmet>
     <ChatApp />
   </div>
 );
